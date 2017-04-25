302b71dae25b0b0618863a6265ab4d82773b11c1
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-04-25 17:41:53 -0400

    import less in js. 0.1.7

--- src/chart-info/chart-info.js
+++ src/chart-info/chart-info.js
@@ -1,6 +1,7 @@
 import Component from 'can-component';
 import view from './template.stache';
 import ViewModel from './view-model';
+import './style.less';
 
 export default Component.extend({
   tag: 'chart-info',
